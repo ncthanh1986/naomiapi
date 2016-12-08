b7742be3f56b376b2914cc168abdd08c864b7f60
Thanh Nguyen <tnguyenuni@gmail.com>
2016-12-09 00:03:01 +1100

    better json2

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -12,14 +12,12 @@ class Api::PostsController < ApplicationController
     def create
         post = Post.new(post_params)
         if post.save
-            render json: {
-                status: 200,
+            render status: 200, json: {
                 message: "Successfully created post",
                 post: post
                 }.to_json
         else
-            render json: {
-                status: 500,
+            render status: 422, json: {
                 errors: post.errors
                 }.to_json
         end
@@ -28,8 +26,7 @@ class Api::PostsController < ApplicationController
     def destroy
         post = Post.find(params[:id])
         post.destroy
-        render json: {
-            status: 200,
+        render status: 200, json: {
             message: "Deleted"
         }.to_json
     end
@@ -37,12 +34,17 @@ class Api::PostsController < ApplicationController
     def update
         post = Post.find(params[:id])
         if post.update(post_params)
-            render json: {
-                status: 200,
+            render status: 200, json: {
                 message: "Successfully updated post",
                 post: post
                 }.to_json
+        else
+            render status: 500, json: {
+                message: "Cannot update",
+                post: post
+                }.to_json
         end
+
     end
 
     private
